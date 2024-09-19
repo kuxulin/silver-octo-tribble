024368d43ac1cc2e373214dd3248f8ba35eefb2f
Maksym Moroz <maskmoroz@gmail.com>
2024-09-19 08:36:08 +0300

    created simple controller

---
+++ Web/Controllers/ProjectController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Contexts;
+using Microsoft.EntityFrameworkCore;
+using Web.ViewModels;
+using Core.Entities;
+
+namespace Web.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ProjectController : ControllerBase
+{
+    private readonly DatabaseContext _context;
+
+    public ProjectController(DatabaseContext context)
+    {
+        _context = context;
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetProjects()
+    {
+        var projects = await _context.Projects.ToListAsync();
+
+        return Ok(projects);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateProject(ProjectCreateVewModel model)
+    {
+        var project = new Project()
+        {
+            Name = model.Name,
+
+        };
+
+        _context.Add(project);
+        await _context.SaveChangesAsync();
+
+        return Ok(project.Id);
+    }
+}
