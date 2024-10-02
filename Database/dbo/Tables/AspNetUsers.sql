eed77653012955aa074f6d69d0058f102bd162a4
Maksym Moroz <maskmoroz@gmail.com>
2024-10-02 11:37:47 +0300

    modified db schema

--- Database/dbo/Tables/AspNetUsers.sql
+++ Database/dbo/Tables/AspNetUsers.sql
@@ -9,13 +9,14 @@
     [PasswordHash]         NVARCHAR (MAX)     NULL,
     [SecurityStamp]        NVARCHAR (MAX)     NULL,
     [ConcurrencyStamp]     NVARCHAR (MAX)     NULL,
-    [PhoneNumber]          NVARCHAR (MAX)     NULL,
+    [PhoneNumber]          NVARCHAR (16)     NULL,
     [PhoneNumberConfirmed] BIT                NOT NULL,
     [TwoFactorEnabled]     BIT                NOT NULL,
     [LockoutEnd]           DATETIMEOFFSET (7) NULL,
     [LockoutEnabled]       BIT                NOT NULL,
     [AccessFailedCount]    INT                NOT NULL,
     [RefreshToken]         NVARCHAR(MAX)      NULL,
+    [FullName] NVARCHAR(100) NULL, 
     CONSTRAINT [PK_AspNetUsers] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
