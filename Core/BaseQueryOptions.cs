0890cd4f710d911cad65a87a9d8b309acf50bace
Maksym Moroz <maskmoroz@gmail.com>
2024-10-03 12:43:40 +0300

    created controller, entitites and mapper

---
+++ Core/BaseQueryOptions.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core;
+public class BaseQueryOptions
+{
+    public int PageSize { get; set; } = 20;
+    public int PageIndex { get; set; } = 0;
+    public string? SortField { get; set; }
+    public bool SortByDescending { get; set; } = false;
+
+    public int GetStartIndex()
+    {
+        return PageIndex * PageSize;
+    }
+}
---
+++ Infrastructure/Mappings/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core.DTOs;
+using Core.Entities;
+
+namespace Infrastructure.Mappings;
+
+class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<User, UserDTO>();
+    }
+}
+
