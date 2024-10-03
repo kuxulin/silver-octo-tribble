5aa5d160055af6aeed8cf702c004d7ec3fc3a54a
Maksym Moroz <maskmoroz@gmail.com>
2024-10-03 22:51:48 +0300

    client: created admin panel with table

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { InitialComponent } from './core/initial/initial.component';
 import { LoginComponent } from './core/login/login.component';
 import { RegisterComponent } from './core/register/register.component';
+import { AdminPanelComponent } from './core/admin-panel/admin-panel.component';
 
 export const routes: Routes = [
   { path: 'initial', component: InitialComponent },
   { path: '', redirectTo: '/initial', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'panel', component: AdminPanelComponent },
 ];
---
+++ client/src/app/core/admin-panel/admin-panel.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { UserService } from '../../shared/services/user.service';
+import { CommonModule } from '@angular/common';
+import { DataSource } from '@angular/cdk/collections';
+import User from '../../shared/models/user';
+import { Observable } from 'rxjs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+@Component({
+  selector: 'app-admin-panel',
+  standalone: true,
+  imports: [
+    MatTableModule,
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatPaginatorModule,
+  ],
+  templateUrl: './admin-panel.component.html',
+  styleUrl: './admin-panel.component.scss',
+})
+export class AdminPanelComponent implements OnInit {
+  displayedColumns = [
+    'Username',
+    'First name',
+    'Last name',
+    'Phone number',
+    'Registration date',
+    'Roles',
+    'Is Blocked',
+    'Actions',
+  ];
+
+  users$!: Observable<User[]>;
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.users$ = this.userService.getAllUsers();
+  }
+}
