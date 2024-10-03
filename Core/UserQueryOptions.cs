5054ef238cf80f70c20d81ca5d31e4181109bfcb
Maksym Moroz <maskmoroz@gmail.com>
2024-10-03 13:00:33 +0300

    created query options, dto and role entity

---
+++ Core/UserQueryOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Core;
+public class UserQueryOptions : BaseQueryOptions
+{
+    public string? PartialUserName { get; set; }
+    public string[] FilterRoles { get; set; } = [];
+    public bool? IsBlocked { get; set; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+}
