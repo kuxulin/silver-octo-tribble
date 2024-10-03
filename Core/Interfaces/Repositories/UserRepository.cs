203e80ebec5a6d7deaea46b53e4df78a14210408
Maksym Moroz <maskmoroz@gmail.com>
2024-10-03 13:00:53 +0300

    created repo

---
+++ Core/Interfaces/Repositories/UserRepository.cs
@@ -0,0 +1,7 @@
+﻿using Core.Entities;
+
+namespace Core.Interfaces.Repositories;
+public interface IUserRepository
+{
+    public IQueryable<User> GetAll();
+}
---
+++ Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using Core.Interfaces.Repositories;
+using Persistence.Data.Contexts;
+
+namespace Persistence.Repositories;
+
+class UserRepository : IUserRepository
+{
+    private readonly DatabaseContext _context;
+
+    public UserRepository(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+    public IQueryable<User> GetAll()
+    {
+        return _context.Users;
+    }
+}
+
