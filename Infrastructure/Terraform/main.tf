dd0aab1ba8974a80e5987cf46dcea6de943b5231
Maksym Moroz <maskmoroz@gmail.com>
2024-11-10 17:39:17 +0200

    fixed typo

--- Infrastructure/Terraform/main.tf
+++ Infrastructure/Terraform/main.tf
@@ -23,7 +23,7 @@ resource "azurerm_key_vault_access_policy" "default" {
   ]
 }
 
-resource "azurerm_key_vault_access_policy" "default" {
+resource "azurerm_key_vault_access_policy" "server_policy" {
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = azurerm_windows_web_app.server.id
   key_vault_id = azurerm_key_vault.res-1
