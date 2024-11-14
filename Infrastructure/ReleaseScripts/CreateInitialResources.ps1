8ee4dcb3c8f6f283e28f1b314e158d7e8f7489a1
Maksym Moroz <maskmoroz@gmail.com>
2024-11-14 10:53:49 +0200

    added release scipts folder

---
+++ Infrastructure/ReleaseScripts/CreateInitialResources.ps1
@@ -0,0 +1,12 @@
+﻿param (
+    [string]$Location,
+    [string]$ResourceGroupName,
+    [string]$StorageAccountName,
+    [string]$DefaultContainerName
+)
+
+az group create --location $Location --name $ResourceGroupName
+
+az storage account create --name $StorageAccountName --resource-group $ResourceGroupName --location $Location --sku Standard_LRS
+
+az storage container create --name $DefaultContainerName --account-name $StorageAccountName
