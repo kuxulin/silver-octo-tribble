03f0bea6e0d1cad169b5ba28f4b13b21b888b6d2
Maksym Moroz <maskmoroz@gmail.com>
2024-11-14 10:59:26 +0200

    added script for retrieving storage key

---
+++ Infrastructure/ReleaseScripts/GetStorageKey.ps1
@@ -0,0 +1,8 @@
+﻿param (
+    [string]$ResourceGroupName,
+    [string]$StorageAccountName
+)
+
+$key = (Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupName -AccountName $StorageAccountName).Value[0]
+
+Write-Host "##vso[task.setvariable variable=StorageKey]$key"
